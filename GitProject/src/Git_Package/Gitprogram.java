4f6fa14537f2197678b35476a9ed26c6b31cb07b
DELL <DELL@DESKTOP-ALEBNJL>
2023-03-14 20:21:45 +0530

    new line added

--- GitProject/src/Git_Package/Gitprogram.java
+++ GitProject/src/Git_Package/Gitprogram.java
@@ -5,6 +5,7 @@ public class Gitprogram {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("welcome");
+		System.out.println("Hello automation");
 	}
 
 }
